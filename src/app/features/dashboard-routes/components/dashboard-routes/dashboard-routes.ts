955b29ebf9ba2368b6d87a13095a16a63eec7f9e
kennyonsig <kennyonsig@gmail.com>
2025-06-13 23:10:11 +0300

    f

--- src/app/features/dashboard-routes/components/dashboard-routes/dashboard-routes.ts
+++ src/app/features/dashboard-routes/components/dashboard-routes/dashboard-routes.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { DashboardRoutesService } from '../../services/dashboard-routes-service';
 import { MatIconModule } from "@angular/material/icon";
 import { IRoutes } from '../../interfaces/iroutes';
@@ -6,7 +6,6 @@ import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-dashboard-routes',
-
   templateUrl: './dashboard-routes.html',
   imports: [
     MatIconModule,
@@ -16,67 +15,12 @@ import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 })
 export class DashboardRoutes {
   routesService = inject(DashboardRoutesService);
-  displayedCount = signal(10);
-
-  rawRoutes = this.routesService.routesData;
-
-  sortKey = signal<keyof IRoutes | null>(null);
-  sortDirection = signal<'asc' | 'desc' | null>(null);
-
-  sortedRoutes = computed(() => {
-    const key = this.sortKey();
-    const direction = this.sortDirection();
-    const routes = this.rawRoutes();
-
-    if (!key || !direction) return routes;
-
-    return [...routes].sort((a, b) => {
-      const valA = a[key];
-      const valB = b[key];
-
-      if (typeof valA === 'string' && typeof valB === 'string') {
-        return direction === 'asc'
-          ? valA.localeCompare(valB)
-          : valB.localeCompare(valA);
-      }
-
-      if (typeof valA === 'number' && typeof valB === 'number') {
-        return direction === 'asc' ? valA - valB : valB - valA;
-      }
-
-      return direction === 'asc'
-        ? String(valA).localeCompare(String(valB))
-        : String(valB).localeCompare(String(valA));
-    });
-  });
-
-  sortedRoutesLength = computed(() => this.sortedRoutes().length);
-
-  displayedRoutes = computed(() =>
-    this.sortedRoutes().slice(0, this.displayedCount())
-  );
-
-  onScroll() {
-    if (this.displayedCount() < this.sortedRoutesLength()) {
-      this.displayedCount.update(value =>
-        Math.min(value + 10, this.sortedRoutesLength())
-      );
-    }
-  }
 
   sortColumn(column: keyof IRoutes) {
-    if (this.sortKey() === column) {
-      this.sortDirection.update(dir =>
-        dir === 'asc' ? 'desc' :
-          dir === 'desc' ? null : 'asc'
-      );
+    this.routesService.sortColumn(column);
+  }
 
-      if (this.sortDirection() === null) {
-        this.sortKey.set(null);
-      }
-    } else {
-      this.sortKey.set(column);
-      this.sortDirection.set('asc');
-    }
+  onScroll() {
+    this.routesService.onScroll();
   }
 }
--- src/app/features/dashboard-routes/services/dashboard-routes-service.ts
+++ src/app/features/dashboard-routes/services/dashboard-routes-service.ts
@@ -1,12 +1,102 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import routesData from '../../assets/routesData.json'
+import { IRoutes } from '../interfaces/iroutes';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardRoutesService {
-
   routesData = signal(routesData);
+  sortKey = signal<keyof IRoutes | null>(null);
+  sortDirection = signal<'asc' | 'desc' | null>(null);
+  displayedCount = signal(10);
 
+  getIpNumber(ip: string): number {
+    const octets = ip.split('.').map(Number);
+    return ((octets[0] << 24) | (octets[1] << 16) | (octets[2] << 8) | octets[3]) >>> 0;
+  }
+
+  getAddressPart(route: IRoutes) {
+    const {address, mask} = route;
+    const ipNumber = this.getIpNumber(address);
+    const maskValue = Number(mask);
+
+    return {
+      network: ipNumber,
+      mask: maskValue
+    };
+  }
+
+  sortedRoutes = computed(() => {
+    const key = this.sortKey();
+    const direction = this.sortDirection();
+    const routes = this.routesData();
+
+    if (!key || !direction) return routes;
+
+    return [...routes].sort((a, b) => {
+      if (key === 'address') {
+        const addressA = this.getAddressPart(a);
+        const addressB = this.getAddressPart(b);
+
+        if (addressA.network !== addressB.network) {
+          return direction === 'asc'
+            ? addressA.network - addressB.network
+            : addressB.network - addressA.network;
+        }
+
+        if (addressA.mask !== addressB.mask) {
+          return direction === 'asc'
+            ? addressA.mask - addressB.mask
+            : addressB.mask - addressA.mask;
+        }
+
+      } else if (key === 'gateway') {
+        const gatewayA = this.getIpNumber(a.gateway);
+        const gatewayB = this.getIpNumber(b.gateway);
+        return direction === 'asc'
+          ? gatewayA - gatewayB
+          : gatewayB - gatewayA;
+      }
+
+      return direction === 'asc'
+        ? a[key].localeCompare(b[key])
+        : b[key].localeCompare(a[key]);
+    });
+  });
+
+  sortedRoutesLength = computed(() => this.sortedRoutes().length);
+
+  displayedRoutes = computed(() =>
+    this.sortedRoutes().slice(0, this.displayedCount())
+  );
+
+
+  sortColumn(column: keyof IRoutes) {
+    const isCurrentColumn = this.sortKey() === column;
+
+    if (isCurrentColumn) {
+      this.sortDirection.update(dir => {
+        if (dir === 'asc') return 'desc';
+        if (dir === 'desc') return null;
+        return 'asc';
+      });
+
+      if (this.sortDirection() === null) {
+        this.sortKey.set(null);
+      }
+    } else {
+      this.sortKey.set(column);
+      this.sortDirection.set('asc');
+    }
+  }
+
+  onScroll() {
+    if (this.displayedCount() < this.sortedRoutesLength()) {
+      this.displayedCount.update(value =>
+        Math.min(value + 10, this.sortedRoutesLength())
+      );
+    }
+  }
 }
